cf8aadfcd4d05419a695747edb13af992c1fe104
Soham Kukreti <kukretisoham@gmail.com>
2025-03-29 22:49:52 +0530

    Opens website in the same tab

--- background.js
+++ background.js
@@ -2,7 +2,13 @@ chrome.omnibox.onInputEntered.addListener((text) => {
     chrome.storage.sync.get("aliases", (data) => {
       const aliases = data.aliases || {};
       const url = aliases[text] || `https://www.google.com/search?q=${text}`;
-      chrome.tabs.create({ url });
+      
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const currentTab = tabs[0];
+        if (currentTab && currentTab.id) {
+          chrome.tabs.update(currentTab.id, { url });
+        }
+      });
     });
   });
   
