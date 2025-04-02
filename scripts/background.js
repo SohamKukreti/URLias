a5a410bd97336b21710faf4e3f1c1b9cd414bfcd
Soham Kukreti <kukretisoham@gmail.com>
2025-04-02 12:31:32 +0530

    Update name and icon, Allow users to add basic parameters to aliases The user can now do go gh user and they will be able to visit github.com/user, similarly would work for any url where the parameter can be directly appended to the url To do: Make it work with different types of urls, allow users to set their own parameterized urls and edit them

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,27 @@
 chrome.omnibox.onInputEntered.addListener((text) => {
+
+  const parts = text.trim().split(/\s+/);
+  const aliasKey = parts[0];
+  const extraQuery = parts.slice(1).join(" ");
+  console.log(text)
+  console.log(parts)
   chrome.storage.sync.get("aliases", (data) => {
     const aliases = data.aliases || {};
-    let url = aliases[text] || `https://www.google.com/search?q=${text}`;
-    
+
+    let url = aliases[aliasKey];
+
+    if (url) {
+      if (extraQuery) {
+        url = url.replace(/\/+$/, "");
+        url += "/" + encodeURIComponent(extraQuery);
+      }
+    } else {
+      url = `https://www.google.com/search?q=${encodeURIComponent(text)}`;
+    }
+
     url = normalizeUrl(url);
+
+    
 
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
